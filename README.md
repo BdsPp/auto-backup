46e951d5a84bc9a860a35a7c005b8e1b465e304e
Sueqkjs <sueqk@outlook.jp>
2021-05-09 22:09:46 +0900

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# BdsPp-auto-backup
+
+ This plugin will automatically back up your BdsPp world.
+ 
+## Preparation
+
+ Within BdsPp.
+```bash
+ cd plugins
+ git clone https://github.com/BdsPp/auto-backup
+ cd auto-backup
+ npm i
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs-extra');
+module.exports = {
+    Handler: function(api, logger){
+        api.on('ready', () => {
+            logger.Info('ready!');
+            logger.Info('This plugin will automatically back up your BdsPp world for you! You need the built-in plugin \'ecchi\'.');
+        });
+        api.on('ecchi.15minuteEvent', () => {
+            const name = 'backup-' + Date.now();
+            fs.copySync('../worlds/main', '../worlds/' + name);
+            logger.Info("I backed the World. The file name is '" + name + "'.");
+        });
+    },
+    Version: '1.0.0'
+}
