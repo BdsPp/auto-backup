60f1eb5039984ce6d978c6a1df7979b587668562
FurenjiCH <furenjich@gmail.com>
2021-05-10 09:36:16 +0900

    Changed to use api.onTimer (Plugin API)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const fs = require('fs-extra');
 module.exports = {
-    Handler: function(api, logger){
+    Handler: function(api, logger) {
         api.on('ready', () => {
             logger.Info('ready!');
             logger.Info('This plugin will automatically back up your BdsPp world for you! You need the built-in plugin \'ecchi\'.');
         });
-        api.on('ecchi.15minuteEvent', () => {
+        api.onTimer("15m", () => {
             const name = 'backup-' + Date.now();
             fs.copySync('../../worlds/main', '../../worlds/' + name);
             logger.Info("I backed the World. The file name is '" + name + "'.");
